08a626e9666699d8d61d433ea2f4137a9a731061
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-10-01 22:13:35 +0300

    npm yup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,10 @@
-import { useFormik } from 'formik';
+// import { nanoid } from 'nanoid'
+// model.id = nanoid() //=> "V1StGXR8_Z5jdHi6B-myT"
+import { ContactForm } from './ContactsForm/ContactsForm';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework phonebook
+    <div>
+      <ContactForm />
     </div>
   );
 };
